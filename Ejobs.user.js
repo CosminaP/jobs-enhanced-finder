78e14c8ff23ca338c97d32f5175ceaa632c1c12f
cosilynaweb <cosmina.cosilyn@gmail.com>
2015-07-05 15:57:30 +0300

    Code Formating

--- Ejobs.user.js
+++ Ejobs.user.js
@@ -9,18 +9,27 @@
 // ==/UserScript==
 
 function filterJobs() {
-   var jobTitle = document.getElementsByClassName("title"),  
-	   reg = /javascript|Front.?end|web designer/i;	
-    for( var i = 0;  i< jobTitle.length ; i++){
-        var a = jobTitle[i],
-            li = a.parentNode.parentNode.parentNode.parentNode,
-            jobName = a.innerHTML;				      
-        if ( "LI" === li.tagName  && jobName.search(reg) === -1) {			
-				li.style.display="none";
-        }
+  var jobTitle = document.getElementsByClassName("title"),
+	  reg = /javascript|Front.?end|web designer/i;	
+  for (var i = 0; i < jobTitle.length; i++) {
+    var a = jobTitle[i],
+		li = a.parentNode.parentNode.parentNode.parentNode,
+		jobName = a.innerHTML;				      
+    if ("LI" === li.tagName && jobName.search(reg) === -1) {
+      li.style.display="none";
     }
+  }
 }
+
 window.onload = function () {
-	var buttonFilter = $("<button>Filtreaza Joburi</button>").css({"font-size" : "14px" , "color" : "#FFF" , "background-color" : "transparent","width": "130px","border": "none","font-family": "Roboto, Arial, sans-serif","padding": "13px 0 0 5px"}).click(filterJobs);
-	$("#navbar").append(buttonFilter);
+  var buttonFilter = $("<button>Filtreaza Joburi</button>").css({
+    "font-size": "14px", 
+	"color": "#FFF",
+	"background-color": "transparent",
+	"width": "130px",
+	"border": "none",
+	"font-family": "Roboto, Arial, sans-serif",
+	"padding": "13px 0 0 5px"
+  }).click(filterJobs);
+  $("#navbar").append(buttonFilter);
 };
